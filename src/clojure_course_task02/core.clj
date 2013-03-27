3e7c437ef5b06ec7b7ef1702e94c15800b763e9d
ks <ks@jspcj.com>
2013-03-28 00:50:01 +0400

    башка не варит

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,23 +1,51 @@
-(ns clojure-course-task02.core
-  (:gen-class)
-  (:import java.io.File))
-
+; # описание типов комментов
 ; pre-release
 ;; pre-pre-release
 ;;; super-base-primitive-example
 
+; # регэкспы
 ;;; (#(re-pattern %) "dsflkjhsfs")
-(import '(java.io File))
+
+; # достаем список файлов
 ;; (def files (.listFiles (File. ".")))
 ;;; (.toString (get files 3))
 ;; (pmap #(get files %) '(1 2))
 ; (pmap  #(.toString (get files %)) (take (count files) (iterate inc 0)))
 
+; # получаем количетво доп аргументов 
+;; (defn xz [a & args]
+;;   (println (sort (flatten (conj a args))))
+;;   (if (not (= args nil)) (count (vec args))))
+
+
+;# получаем список вида ({путь1:пройден},{путь2:nil}, ...)
+;; (defn walk-files-from-path [path]
+;;   (loop
+;;       [files (get-files-by-path path)
+;;        res []
+;;   nil)))
+
+
+(ns clojure-course-task02.core
+  (:gen-class)
+  (:import java.io.File))
+
+;# получаем все файлы в указанном пути
 (defn get-files-by-path [path]
   (pmap #(.getName %) (seq (.listFiles (File. path)))))
 
-(defn walk-files-from-path [path]
-  nil)
+;# проверка ненулевых
+(defn not-nil? [val]
+  (if (nil? val) false
+      true))
+
+(defn get-files-tree-recur [start-path]
+  (loop [pre-res (vec (get-files-by-path start-path))
+        res []]
+    (if (= (filter not-nil? (flatten pre-res)) res)
+      res
+      (recur (conj res (flatten pre-res)) (pmap get-files-tree-recur res)))))
+    
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
