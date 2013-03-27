9e3048601443c8f5eb02712ae590949953a15487
ks <ks@ks-work.(none)>
2013-03-27 20:00:39 +0400

    первыйнах

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,10 +1,24 @@
 (ns clojure-course-task02.core
-  (:gen-class))
+  (:gen-class)
+  (:import java.io.File))
 
+; pre-release
+;; pre-pre-release
+;;; super-base-primitive-example
+
+;;; (#(re-pattern %) "dsflkjhsfs")
+(import '(java.io File))
+;; (def files (.listFiles (File. ".")))
+;;; (.toString (get files 3))
+;; (pmap #(get files %) '(1 2))
+; (pmap  #(.toString (get files %)) (take (count files) (iterate inc 0)))
 
 (defn find-files [file-name path]
   "TODO: Implement searching for a file using his name as a regexp."
-  nil)
+  (let [
+       files (.listFiles (File. path))
+  ])
+  (pmap #(.getName %) (seq (.listFiles (File. path)))))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
@@ -16,3 +30,5 @@
     (do
       (println "Searching for " file-name " in " path "...")
       (dorun (map println (find-files file-name path))))))
+
+(-main "a" ".")
